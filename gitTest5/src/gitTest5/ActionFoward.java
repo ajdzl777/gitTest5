510745955576e6c884c3fdeae001740e35971e6b
Sasa <ajdzl777@naver.com>
2019-03-27 10:20:46 +0900

    action foward

---
+++ gitTest5/src/gitTest5/ActionFoward.java
@@ -0,0 +1,22 @@
+package gitTest5;
+
+public class ActionFoward {
+	
+	private String path;
+	private String isRedirect;
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getIsRedirect() {
+		return isRedirect;
+	}
+	public void setIsRedirect(String isRedirect) {
+		this.isRedirect = isRedirect;
+	}
+	
+	
+
+}
